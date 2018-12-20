d40f9dc769b57311569bc4e03ad765d3f14e25ba
The Bad Coder <ghana.bambale@gmail.com>
2018-12-20 06:55:11 +0530

    use of lambda functions

---
+++ lambda_functions.py
@@ -0,0 +1,13 @@
+'''
+Writing functions in python without any name i.e lambda f
+unctions
+'''
+g = lambda x: 3*x +1
+
+#For argument 3 answer should be 10 as below
+
+'''
+How to run a function through terminal-
+python -c 'from lambda_functions import g; print g(3)'
+10
+'''
